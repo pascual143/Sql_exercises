4aa0f2a40c4654713203660fcb7bb666e8456b98
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-05-22 20:12:31 +0200

    Update NivelIntermedio.sql

--- NivelIntermedio.sql
+++ NivelIntermedio.sql
@@ -16,3 +16,22 @@ SELECT titulo
 FROM libros
 ORDER BY LEN(titulo) DESC
 LIMIT 1
+
+6. SUBCONSULTAS
+--Mostrar libros con autor que ha escrito otro libro con fecha posterior
+SELECT libros1.titulo, libros1.autor
+FROM libros libros1
+WHERE libros1.autor_id IN (
+  SELECT autor_id
+  FROM libros libros2
+  WHERE libros2.fecha_publicacion > libros1.fecha_publicacion
+);
+
+-- Encontrar autor con más libros publicados en un año determinado:
+SELECT autores.nombre, COUNT(*) AS libros_publicados_en_anio
+FROM libros
+JOIN autores ON libros.autor_id = autores.id
+WHERE libros.fecha_publicacion >= '2020'
+GROUP BY autores.nombre
+ORDER BY libros_publicados_en_anio DESC
+LIMIT 1;
